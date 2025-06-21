103de77f613cd6de70b1e6e19148740a8b189ded
majidonaisy <onaisym4@gmail.com>
2025-06-21 22:41:17 +0300

    message

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Use SQLite for simplicity - replace with your preferred database URL
-DATABASE_URL = "mssql+pyodbc://sa:s%40DB-utilities-server%4001100101%23@10.50.66.193:1433/your_new_database_name?driver=ODBC+Driver+17+for+SQL+Server&Encrypt=yes&TrustServerCertificate=yes&MultipleActiveResultSets=True"
+DATABASE_URL = "mssql+pyodbc://sa:s%40DB-utilities-server%4001100101%23@10.50.66.193:1433/Distribution?driver=ODBC+Driver+17+for+SQL+Server&Encrypt=yes&TrustServerCertificate=yes&MultipleActiveResultSets=True"
 
 # Create engine
 engine = create_engine(
